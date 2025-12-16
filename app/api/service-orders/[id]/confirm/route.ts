5d1adf0b50ab2b64da487ec49f119df4d25ce2a6
tiagodexheimer <tiagodexheimer@gmail.com>
2025-12-16 14:27:01 -0300

    fix api-service-order-id-confirm

--- app/api/service-orders/[id]/confirm/route.ts
+++ app/api/service-orders/[id]/confirm/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { prisma } from '@/app/lib/prisma';
+import { prisma } from '../../../../lib/prisma';
 
 export async function PATCH(
     request: Request,
